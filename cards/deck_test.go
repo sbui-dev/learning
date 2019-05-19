638ba73519d2d89786b3317dbc1a2ab9424d5ccb
engineer-sbui <sbui.513@gmail.com>
2019-05-19 11:33:50 -0700

    add unit testing for deck and file funcs

---
+++ cards/deck_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 52 {
+		t.Errorf("Expected deck length of 52, but got %v", len(d))
+	}
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card to be Ace of Spades, but got %v", d[0])
+	}
+	if d[len(d)-1] != "King of Clubs" {
+		t.Errorf("Expected first card to be King of Clubs, but got %v", d[0])
+	}
+}
+
+func TestSaveLoadFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected deck length of 52, but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
+}
