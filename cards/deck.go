56c58ba12f837698e2c021d80f6494b0e21c02c0
engineer-sbui <sbui.513@gmail.com>
2019-05-19 11:00:50 -0700

    refactor func positions and add comments

--- cards/deck.go
+++ cards/deck.go
@@ -10,6 +10,7 @@ import (
 // Create a new type of deck which is a slice of strings
 type deck []string
 
+// create a new deck of playing cards
 func newDeck() deck {
 	cards := deck{}
 	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
@@ -23,24 +24,7 @@ func newDeck() deck {
 	return cards
 }
 
-func (d deck) print() {
-	for i, card := range d {
-		fmt.Println(i, card)
-	}
-}
-
-func deal(d deck, handSize int) (deck, deck) {
-	return d[:handSize], d[handSize:]
-}
-
-func (d deck) toString() string {
-	return strings.Join([]string(d), ",")
-}
-
-func (d deck) saveToFile(filename string) error {
-	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
-}
-
+// Create a new deck of playing cards from a file
 func newDeckFromFile(filename string) deck {
 	bs, err := ioutil.ReadFile(filename)
 
@@ -51,4 +35,26 @@ func newDeckFromFile(filename string) deck {
 
 	s := strings.Split(string(bs), ",")
 	return deck(s)
+}
+
+// Deals
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+// Prints the contents of the deck
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+// Returns a comma seperated string of the contents of the deck
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+// Saves deck contents into a file
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
