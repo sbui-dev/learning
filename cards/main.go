291a2f4fd800a85a17a39b55b392561616fc0b4d
engineer-sbui <sbui.513@gmail.com>
2019-05-19 00:39:31 -0700

    add saveToFile and readDeckFromFile funcs

--- cards/main.go
+++ cards/main.go
@@ -1,8 +1,6 @@
 package main
 
 func main() {
-	cards := newDeck()
-	//hand, remainingDeck := deal(cards, 5)
-
-	cards.saveToFile("my_cards")
+	cards := newDeckFromFile("my_cards")
+	cards.print()
 }
