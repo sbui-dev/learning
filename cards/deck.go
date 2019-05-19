0a902a96dafa455d93236fbab95342b020f61b03
engineer-sbui <sbui.513@gmail.com>
2019-05-19 11:01:31 -0700

    add shuffle func to shuffle deck

--- cards/deck.go
+++ cards/deck.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // Create a new type of deck which is a slice of strings
@@ -40,6 +42,18 @@ func newDeckFromFile(filename string) deck {
 // Deals
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+// Shuffles the order of the playing cards inside the deck
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
 
 // Prints the contents of the deck
