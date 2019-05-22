7e3c2970b0e2c6c362f3d64933a3785f885bcf30
engineer-sbui <sbui.513@gmail.com>
2019-05-21 22:09:13 -0700

    print out entire msg to stdout

--- interfaces_http/main.go
+++ interfaces_http/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
+type logWriter struct{}
+
+func main() {
+	resp, err := http.Get("http://google.com")
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	lw := logWriter{}
+
+	//io.Copy(os.Stdout, resp.Body)
+
+	// custom method for the above
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+
+	return len(bs), nil
 }
