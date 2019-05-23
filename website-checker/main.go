2d90cc60bb8cbbf425f55c09b843e573cc998a28
engineer-sbui <sbui.513@gmail.com>
2019-05-22 23:00:32 -0700

    add go routines and channel

--- website-checker/main.go
+++ website-checker/main.go
@@ -1,15 +1,42 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
 	links := []string{
 		"http://google.com",
 		"http://facebook.com",
 		"http://stackoverflow.com",
 		"http://golang.org",
-		"httpL//amazon.com",
+		"http://amazon.com",
 	}
+
+	// create a channel
+	c := make(chan string)
 
 	for _, link := range links {
-
+		// go routine
+		go checkLink(link, c)
 	}
+
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
+	}
+}
+
+func checkLink(link string, c chan string) {
+
+	_, err := http.Get(link)
+
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		c <- "might be down"
+		return
+	}
+
+	fmt.Println(link, "is up")
+	c <- "it's up"
 }
