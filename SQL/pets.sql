edc07a87c906b5d355729d73056c40a2eab3543f
engineer-sbui <sbui.513@gmail.com>
2019-06-23 21:39:56 -0700

    insert owner and pet data. modify/delete data afterward

--- SQL/pets.sql
+++ SQL/pets.sql
@@ -41,5 +41,27 @@ SELECT * FROM owners;
 ALTER TABLE owners
 ALTER COLUMN last_name TYPE VARCHAR(50);
 
-DROP TABLE pets;
-DROP TABLE owners;
+-- insert data
+
+INSERT INTO owners (first_name, last_name, city, state, email)
+VALUES ('Samuel', 'Smith', 'Boston', 'MA', 'samsmith@gmail.com'),
+('Emma', 'Johnson', 'Seattle', 'WA', 'emjohnson@gmail.com'),
+('John', 'Oliver', 'New York', 'NY', 'johnoliver@gmail.com'),
+('Olivia', 'Brown', 'San Francisco', 'CA', 'oliviabrown@gmail.com'),
+('Simon', 'Smith', 'Dallas', 'TX', 'sismith@gmail.com'),
+(null, 'Maxwell', null, 'CA', 'lordmaxwell@gmail.com');
+
+SELECT * FROM owners;
+
+INSERT INTO pets (species, full_name, age, owner_id)
+VALUES ('Dog', 'Rex', 6, 1), ('Rabbit', 'Fluffy', 2, 5), ('Cat', 'Tom', 8, 2),
+('Mouse', 'Jerry', 2, 2), ('Dog', 'Biggles', 4, 1), ('Tortoise', 'Squirtle', 42, 3);
+
+SELECT * FROM pets;
+
+UPDATE pets
+SET age = 3
+WHERE full_name = 'Fluffy';
+
+DELETE FROM owners
+WHERE last_name = 'Maxwell';
